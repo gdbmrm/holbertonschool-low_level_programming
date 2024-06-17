31833ad87fb3a90191c1e520638228ba9e5b32b3
gdbmrm <>
2024-06-17 11:36:50 +0000

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,9 +15,9 @@ long long longlongType;
 char charType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }

22d5734c09b69d7ac5ede378daf8582e98cad9bc
gdbmrm <>
2024-06-18 13:06:53 +0000

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Write a program that prints the alphabet
+ * in lowercase, followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char *str = "abcdefghijklmnopqrstuvwxyz";
+        int i = 26;
+
+        while (i != 0)
+        {
+		putchar(str[i]);
+		i--;
+	}
+
+        putchar('\n');
+        return (0);
+}

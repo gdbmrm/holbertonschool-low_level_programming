607e1bc200550222a8077152a677d88c7b086d84
gdbmrm <>
2024-06-18 14:46:56 +0000

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Write a program that prints the alphabet
+ * in lowercase, followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i = 0;
+
+        while (i < 10)
+        {       putchar(48 + i);
+		putchar(',');
+		putchar(' ');
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}

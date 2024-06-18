52e593d4f474dc7eab5108210fb1360655e120df
gdbmrm <>
2024-06-18 14:06:28 +0000

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Write a program that prints the alphabet
+ * in lowercase, followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char *chiffres = "0123456789";
+        char *lettres = "abcdef";
+        int i = 0;
+        int b = 0;
+
+        while (i < 10)
+        {       putchar(chiffres[i]);
+                i++;
+        }
+        while (b < 6)
+        {       putchar(lettres[b]);
+                b++;
+        }
+
+        putchar('\n');
+        return (0);
+}

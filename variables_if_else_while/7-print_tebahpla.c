3f1f00a2c464ebe7143306ef73a25258326773fa
gdbmrm <>
2024-06-18 13:50:52 +0000

    task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-        char *str = "abcdefghijklmnopqrstuvwxyz";
-        int i = 26;
+	char *str = "abcdefghijklmnopqrstuvwxyz";
+	int i = 25;
 
-        while (i != 0)
-        {
+	while (i != 0)
+	{
 		putchar(str[i]);
 		i--;
 	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }

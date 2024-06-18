ba1b00167e6e0ce06e6841ac6f52db7534f247b9
gdbmrm <>
2024-06-18 12:28:33 +0000

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        while (i < 11)
-        {       putchar(48 + i);
-                i++;
-        }
-        putchar('\n');
-        return (0);
+	while (i < 10)
+	{	putchar(48 + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }

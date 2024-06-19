c4975fb49ab47c33eee7945b9aaa5fd6367f9e10
gdbmrm <>
2024-06-19 12:17:21 +0000

    task

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,28 +1,23 @@
 #include <unistd.h>
 #include <stdio.h>
-#include "0-putchar.c"
+#include "main.h"
+#include "_putchar"
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 int main(void)
 {
-	char *str = "_putchar";
-	int len = sizeof(*str);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (len >= 0)
-	{
-		_putchar(*str);
-		len--;
-
-	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
-}
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/** fonction main pour ex 0 putchar
+ **/
+int main(void)
+{
+	0_putchar();
+	return (0);
+}

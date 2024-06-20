dce44c7b9c803f4341fd14d9c80021aec26178ba
gdbmrm <>
2024-06-20 07:57:46 +0000

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *main prints_putchar
- * return :0s
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet using the _putchar function and
+ * then prints a newline character.
+ *
+ * Return: 0 if the program executes successfully.
  */
 
 int main(void)

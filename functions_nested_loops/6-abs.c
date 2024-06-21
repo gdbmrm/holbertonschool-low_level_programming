d90cf81779e2b1a73dcb30e829c70780cb07c898
gdbmrm <>
2024-06-21 08:03:41 +0000

    task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,5 +1,19 @@
 #include "main.h"
-
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @num: The integer whose absolute value is to be computed.
+ *
+ * Description:
+ * This function takes an integer as input
+ * and returns its absolute value.
+ * If the input integer is negative,
+ * the function returns its positive equivalent.
+ * If the input integer is zero or positive,
+ * the function returns the input integer itself.
+ *
+ * Return: The absolute value of the input integer.
+ */
 
 
 int _abs(int num)

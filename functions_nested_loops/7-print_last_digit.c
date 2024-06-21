2bc2c5b2d94b470da13254056fb43497f646581d
gdbmrm <>
2024-06-21 09:12:24 +0000

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
- *
- *
- *
+ * print_last_digit - fonction qui imprime le dernier chiffre
+ * Description: 'Write a function that prints the last digit of a number.'
+ * @num: param√tres
+ * Return: le dernier chiffre de num
  */
-
 
 int print_last_digit(int num)
 {
-	int last_digit = num % 10;
+	int last_digit;
 
-	_putchar(last_digit);
+	last_digit = num % 10;
 
-	return (0);
+	if (num < 0)
+	{
+		num = -num;
+		last_digit = -last_digit;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }

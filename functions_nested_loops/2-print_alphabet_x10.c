24d46711ba775f36384f9d391593503540dbb1fa
gdbmrm <>
2024-06-21 14:53:39 +0000

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,39 @@
 #include "main.h"
 /**
+ *print_alphabet - Prints the alphabet followed by a newline.
+ *
+ * This function iterates over the alphabet characters and prints each
+ * character using the _putchar function, followed by a newline character.
+*/
+
+void print_alphabet(void)
+{
+	char *str = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
+
+	while (x < 26)
+	{
+		_putchar(str[x]);
+		x++;
+	}
+	_putchar('\n');
+
+}
+
+/**
  * print_alphabet_x10 - Prints the alphabet followed by a newline.
  *
  * This function iterates over the alphabet characters and prints each
  * character using the _putchar function, followed by a newline character.
  * 10 times
+ *
  */
-
 void print_alphabet_x10(void)
 {
-	int n = 0;
-	char *str = "abcdefghijklmnopqrstuvwxyz";
-	int x = 0;
+	int n;
 
-	while (n <= 10)
+	for (n = 0; n < 10; n++)
 	{
-		while (x < 26)
-		{
-			_putchar(str[x]);
-			x++;
-		}
-		n++;
+		print_alphabet();
 	}
-        _putchar('\n');
 }

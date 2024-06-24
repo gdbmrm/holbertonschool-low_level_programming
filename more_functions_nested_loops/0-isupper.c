01ad88b763473696d5c73ba3e4a9ab2d7253d075
gdbmrm <>
2024-06-24 08:21:22 +0000

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,22 +2,11 @@
 
 int _isupper(int c)
 {
-	int upper = 0;
-	int lower = 0;
 
 	if (c <= 'A' && c >= 'Z')
 	{
-		upper++;
-		_putchar('A');
-		_putchar(':');
-		_putchar(upper);
-	
+		return (1);	
 	}
-	lower++;
-	_putchar('a');
-	_putchar(':');
-	_putchar(lower);
-	
 	return (0);
 
 }

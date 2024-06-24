4942d0448c9b3c019b13bacff54a7f4ee5fe8600
gdbmrm <>
2024-06-24 12:14:37 +0000

    task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,26 @@
 #include "main.h"
+/**
+ * print_line - fonction qui crÃe des ligns avec @n tirets
+ *
+ * Write a function that draws a straight line in the terminal.
+ *
+ * @n: nombre de tirets
+ */
 
 void print_line(int n)
 {
 	int a = 0;
 
-	if (n >= 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
 	{
 		while (a != n)
 		{
 			_putchar('_');
 			a++;
 		}
-	_putchar('\n');
-
+		_putchar('\n');
 	}
 }

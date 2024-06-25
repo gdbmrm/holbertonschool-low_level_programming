7502730767e54abb14b03443644260093f915f70
gdbmrm <>
2024-06-25 11:43:32 +0000

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,15 @@
 #include "main.h"
+#include "_putchar.c"
+/**
+ * reset_to_98 - fonction qui modifie la valeur d'un pointeur
+ *
+ * Write a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98.
+ *
+ * @n: variable dont on doit modifier la valeur
+ */
 
 void reset_to_98(int *n)
 {
-	int **p = &n;
-
-	**p = 98;
-	_putchar('p');
+	*n = 98;
 }

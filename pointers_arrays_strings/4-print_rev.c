5096ab44c6ae0e06205d990466306e2e1fab7633
gdbmrm <>
2024-06-25 12:32:18 +0000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - affiche la string en ordre inverse
+ *
+ * Write a function that prints a string, in reverse, followed by a new line.
+ *
+ * @s: string a afficher dans le sens inverse
+ */
+
+void print_rev(char *s)
+{
+
+	int len;
+	int n;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	for (n = len; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}

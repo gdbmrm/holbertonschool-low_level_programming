2fc7bfee48aac39356360c534cd83fa44adf6a5a
gdbmrm <>
2024-06-27 07:52:13 +0000

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,21 +12,18 @@
 
 void print_array(int *a, int n)
 {
-	int x;
+	int x = 0;
 
 	while (x < n)
 	{
-		printf("%d, ", a[x]);
 		if (x == (n - 1))
 		{
 			printf("%d", a[x]);
+		} else
+		{
+			printf("%d, ", a[x]);
 		}
 		x++;
 	}
 	printf("\n");
-
-
-
-
-
 }

b110a6c844caf2c07b999a3d020d4da412226d2c
gdbmrm <>
2024-06-28 13:06:45 +0000

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -21,7 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (j = 0; dest[j] != '\0'; j++)
 	{
 	}
-	if (j < n)
+	if (j <= n)
 	{
 		return (start_dest);
 	}
@@ -31,10 +31,10 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (i = 0; i < n; ++i)
 	{
+		
 		*dest = *src;
 		dest++;
 		src++;
 	}
 	return (start_dest);
-
 }

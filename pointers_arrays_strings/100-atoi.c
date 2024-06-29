50a8602d64c725f38e8f997081bbb536b0e251a0
gdbmrm <>
2024-06-29 00:02:46 +0000

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,22 +3,27 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	long int entier;
-	char sign = '+';
+	unsigned int result = 0;
+	int sign = 0;
 
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == '-')
+                {
+                        sign++;
+                }
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign = '-';
+			result = result * 10 + s[i] - '0';
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{		
+				break;
+			}
 		}
-		if ('0' <= s[i] && s[i] <= '9')
-		{
-			entier = entier + (s[i] - '0');
-		} else 
-		{
-			return (entier);
-		}	
 	}
-	return (entier + sign);
+	if (sign % 2 != 0)
+	{
+		result = -result;
+	}
+	return (result);
 }

60f4582d68bf31ba9ac5d560f0529f1042b06e9d
gdbmrm <>
2024-06-29 00:40:28 +0000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,29 @@
 #include "main.h"
+/**
+ * _strncpy - fonction qui reproduit la commande strncpy
+ *
+ * Copie n caractere de src dans dest
+ *
+ *
+ * @dest: Pointeur vers la chaîne de destinatio
+ * où les caractères seront cier
+ *
+ * @src : Pointeur vers la chaîne sourc
+ * a partir de laquelle les caractères seront cpier
+ *
+ * @n: nombre de caractere a copier
+ *
+ * Return: pointeur vers dest
+ */
+
+
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	char *start_dest = dest;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && dest[i] != '\0'; i++)
 	{
 		*dest = *src;
 		dest++;

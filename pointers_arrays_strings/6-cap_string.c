e303a8bab1adbe87881b6265d6752a94c47e2a46
gdbmrm <>
2024-06-30 21:11:11 +0000

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - nom fonction
+ *
+ * Write a function that capitalizes all words of a string.
+ * Separators of words:
+ * space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
+ *
+ * @s: chaine de cara a modifier
+ * Return: la chaine de caractere
+ */
+
+char *cap_string(char *s)
+{
+	int n;
+
+	for (n = 1; s[n] != '\0'; n++)
+	{
+		if ((s[n - 1] == ' ' || s[n - 1] == '\n' ||
+			s[n - 1] == ',' || s[n - 1] == ';' ||
+			s[n - 1] == '.' || s[n - 1] == '!' ||
+			s[n - 1] == '?' || s[n - 1] == '"' ||
+			s[n - 1] == '(' || s[n - 1] == ')' ||
+			s[n - 1] == '{' || s[n - 1] == '}' ||
+			s[n - 1] == '\t') && (s[n] >= 'a' && s[n] <= 'z'))
+		{
+			s[n] -= 32;
+		}
+	}
+	return (s);
+}

2fb725351823d4c2d209c98c746ffc0b1ec0c1a6
gdbmrm <>
2024-06-30 18:12:24 +0000

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * string_toupper - fonction qui passe les lettres en minuscule
+ * a des lettres en majuscules
+ *
+ * Write a function that changes all lowercase letters
+ * of a string to uppercase.
+ *
+ * @n: charactere dont on veut passer les lettres minuscules en majuscules
+ *
+ * Return: n en majuscule
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	for (; n[i] != '\0'; i++)
+	{
+		if (n[i] > 97 && n[i] < 122)
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+
+}

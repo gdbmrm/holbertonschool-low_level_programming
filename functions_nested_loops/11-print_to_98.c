3193a80f3fd33ec5f7e78c7e906ca3534a386522
gdbmrm <>
2024-06-30 22:16:27 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,25 @@ void print_to_98(int n)
 	{
 		for (i = 0; i <= (98 - n); i++)
 		{
-			printf("%d", n + i);
-			if ( 
-
+			if (i == 98)
+			{
+				printf("%d", n + i); 
+			} else
+			{
+				printf("%d, ", n + i);
+			}
+		}
+	} else if (n > 98)
+	{
+		for (i = 0; i <= (n - 98); i++)
+		{
+			 if (i == 98)
+                        {
+                                printf("%d", n - i);
+                        } else
+			{
+				printf("%d, ", n - i);
+			}
 		}
 	}
 	printf("\n");

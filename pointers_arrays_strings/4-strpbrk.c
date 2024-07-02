63d127b3fc34c8f448dc0ee1e5eba968f4bf3ef9
gdbmrm <>
2024-07-02 21:56:37 +0000

    task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,15 @@
 #include "main.h"
-
+/**
+ * _strpbrk - nom de la fonction
+ *
+ * Write a function that searches a string for any of a set of bytes.
+ *
+ * @s: chaine de cara
+ * @accept: string a chercher dans s
+ *
+ * Return:  a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;

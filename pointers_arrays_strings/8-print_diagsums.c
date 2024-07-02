6a2e1a38415e63be505b2f0e41c0ce35d2808c75
gdbmrm <>
2024-07-02 22:02:37 +0000

    task 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_diagsums - nom de la fonction
+ *
+ * Write a function that prints the sum of the
+ * two diagonals of a square matrix of integers.
+ *
+ * @a: tableau castéen int*
+ * @size: taille du tableau
+ *
+ * Return: somme des deux diagonales
+ */
 void print_diagsums(int *a, int size)
 {
 	int i;

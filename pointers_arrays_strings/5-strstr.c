a8f0421abfa1710a3c5807affec56528de1c7eb6
gdbmrm <>
2024-07-02 21:59:40 +0000

    task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stddef.h>
-
+/**
+ * _strstr - nom fonction
+ *
+ * Write a function that locates a substring.
+ * Fonction qui cherche une aiguille dans une botte de foin
+ *
+ * @haystack: botte de foin
+ * @needle: aiguille
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i;

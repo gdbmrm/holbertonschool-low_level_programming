d473e2a8e91c896b8917f491604e0aa020a456c0
gdbmrm <>
2024-07-03 18:54:56 +0000

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,7 +1,17 @@
 #include "main.h"
-
+/**
+ * _print_rev_recursion - nom de la fonction
+ *
+ * Write a function that prints a string in reverse.
+ *
+ * @s: string a inverser
+ */
 void _print_rev_recursion(char *s)
 {
-	
-
+	if (*s <= 0)
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }

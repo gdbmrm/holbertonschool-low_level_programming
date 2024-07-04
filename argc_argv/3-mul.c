49133ae679df4e1b541d4c517c610f54dff74b00
gdbmrm <>
2024-07-04 23:08:57 +0000

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - fonction
+ *
+ * Write a program that multiplies two numbers.
+ * Your program should print the result of the multiplication,
+ * followed by a new line
+ * You can assume that the two numbers and result of the multiplication
+ * can be stored in an integer
+ * If the program does not receive two arguments,
+ *  your program should print Error, followed by a new line, and return 1
+ *
+ *  @argc: nombre d'arguments
+ *  @argv: arguments
+ *
+ *  Return: multiplication des deux arguments
+ */
 int main(int argc, char *argv[])
 {
 	int resultat = 0;
@@ -11,6 +26,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	resultat = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n",resultat);
+	printf("%d\n", resultat);
 	return (resultat);
 }

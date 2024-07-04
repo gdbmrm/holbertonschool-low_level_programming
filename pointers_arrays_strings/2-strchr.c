97b1a1fdd523da701f7ddd97dfbd815fbfdfd029
gdbmrm <>
2024-07-04 11:11:51 +0000

    task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,23 @@
 #include "main.h"
-#include <stddef.h>
+/**
+ * _strchr - nom de la fonction
+ *
+ * Write a function that locates a character in a string.
+ *
+ * @s: string
+ * @c: character a locates
+ *
+ * Return: Returns a pointer to the first occurrence
+ *  of the character c in the string s,
+ * or NULL if the character is not found
+ */
+
+
 
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (*s == '\0')
-	{
-		return ('\0');
-	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -16,6 +25,10 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 
+	}
+	if (c == '\0')
+	{
+		return (s + i);
 	}
 	return ('\0');
 }

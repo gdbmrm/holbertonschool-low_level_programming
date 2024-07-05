ba6190f53f119c3d3f0b5cd13c85fb4ab0b4c3a9
gdbmrm <>
2024-07-05 19:12:04 +0000

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,22 +1,44 @@
 #include "main.h"
-
-int prime(int nn)
+/**
+ * prime - fonction pour savoir si n est un nombre premier
+ *
+ * @n: nombre a savoir si premier ou non
+ * @i: diviseur de n
+ *
+ * Return: 1 si n est un nb premier sinon 0
+ */
+int prime(int n, int i)
 {
-	int x;
-
-	for (x = 0; x < n; x++)
+	if (n <= 1)
 	{
-		if (nn % x == 0 && x != 1)
-		{
-
-
+		return (0);
+	} else if (n == 2)
+	{
+		return (1);
+	} else if (n % i == 0)
+	{
+		return (0);
+	} else if (i * i > n)
+	{
+		return (1);
+	}
+	return (prime(n, i + 1));
 }
+/**
+ * is_prime_number - fonction recursive
+ *
+ * Write a function that returns 1
+ * if the input integer is a prime number, otherwise return 0.
+ *
+ * @n: nombre a savoir si premier ou non
+ *
+ * Return: 1 si premier sinon 0
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
-		return (0)
+		return (0);
 	}
-
-
+	return (prime(n, 2));
 }

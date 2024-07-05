3792ad9c287f1a93cfd9f947864b1a1ef6e67eea
gdbmrm <>
2024-07-05 20:27:04 +0000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -23,7 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	char *start_dest = dest;
 
-	for (i = 0; i < n && dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}

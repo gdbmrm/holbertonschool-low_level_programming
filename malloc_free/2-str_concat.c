af123cfe85618cae3430e3846b19079a5a76221a
gdbmrm <>
2024-07-08 09:49:49 +0000

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - fonction qui imite strcat
+ *
+ * Write a function that concatenates two strings.
+ *
+ * @s1: 1 ere chaine a concatener
+ * @s2: 2eme chaine a concatener
+ *
+ * Return: pointeur vers un tableau avec les deux chaines concat
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	unsigned long int i = 0, j = 0;
+
+	ar = malloc(sizeof(s1) + sizeof(s2));
+
+	while (s1[i] != '\0')
+	{
+		ar[i] = s1[i];
+		i++;
+	}
+	for (j = 0; s2[j] != '\0' && j <= sizeof(s2); j++, i++)
+	{
+		ar[i] = s2[j];
+	}
+	return (ar);
+	free(ar);
+
+}

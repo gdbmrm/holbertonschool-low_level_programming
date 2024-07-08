b783a4a87dc293e8b1b51f3c5c8992b927e7fbd5
gdbmrm <>
2024-07-08 08:29:10 +0000

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - cr√e un tableau et initialise toutes les valeurs a c
+ *
+ * Write a function that creates an array of chars,
+ * and initializes it with a specific char.
+ *
+ * @size: taille du tableau
+ * @c: charactere a initialiser
+ *
+ * Return: pointeur du tableau
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ar = malloc(sizeof(ar) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
+	return (ar);
+	free(ar);
+}

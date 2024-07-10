0c612f6d9286cad98d2ad424f66a76b53d6f4088
gdbmrm <>
2024-07-10 20:39:50 +0000

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ar;
 	unsigned long int i = 0, j = 0;
-	int lens1 = 0, lens2 = 0;
+	unsigned long int lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
 	{
@@ -39,10 +39,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	while (s1[i] != '\0')
+	for (i = 0; i < lens1; i++)
 	{
 		ar[i] = s1[i];
-		i++;
 	}
 	for (j = 0; s2[j] != '\0'; j++, i++)
 	{

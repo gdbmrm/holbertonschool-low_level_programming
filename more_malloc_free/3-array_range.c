3a3bdc7afe65e03b6a6684b8192b77d22d9fff35
gdbmrm <>
2024-07-10 14:50:43 +0000

    task 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -5,22 +5,22 @@ int *array_range(int min, int max)
 {
 	int *ar;
 	int i = 0;
+	int r = (max - min) + 1;
 
 	if (min >= max)
 	{
 		return (NULL);
 	}
-	ar = malloc((max - min) * sizeof(int));
+	ar = malloc(r * sizeof(int));
 
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	for (;i < max; i++)
+	for (;i < r; i++)
 	{
 		ar[i] = min;
 		min++;
-
 	}
 	return (ar);
 

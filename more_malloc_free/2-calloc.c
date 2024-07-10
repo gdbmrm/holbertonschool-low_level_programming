4e51999c000a4f71dae9a80e558e4e415a3f5f81
gdbmrm <>
2024-07-10 12:25:09 +0000

    task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,13 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _calloc - fonction qui imite calloc
+ *
+ * Write a function that allocates memory for an array, using malloc.
+ *
+ * @nmemb: nombre d'elements du tableau
+ * @size: taille en bytes des elements du tab
+ *
+ * Return: tableau de type void allouédynamiquement
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	void *ar;
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
+	ar = malloc(nmemb * size);
 
-
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	return (ar);
 }

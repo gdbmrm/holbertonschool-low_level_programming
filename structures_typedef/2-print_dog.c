c9813fa74a11cf5a236f45756605dd37e53f9d3f
gdbmrm <>
2024-07-11 12:06:51 +0000

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - affiche un chien
+ * @d: structure
+
+ * Description: fonction qui affiche un chien
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name == NULL)
+{
+printf("Name: (Nil)");
+}
+printf("Name: %s\n", d->name);
+
+if (d->age == 0)
+{
+printf("Age: (Nil)");
+}
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+{
+printf("Owner: (Nil)");
+}
+printf("Owner: %s\n", d->owner);
+}

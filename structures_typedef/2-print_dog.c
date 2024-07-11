55163c864d629824d922cc1385e02e6ac5738320
gdbmrm <>
2024-07-11 12:15:31 +0000

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,14 +14,14 @@ return;
 }
 if (d->name == NULL)
 {
-printf("Name: (Nil)");
-}else
+printf("Name: (Nil)\n");
+} else
 {
 printf("Name: %s\n", d->name);
 }
 if (d->age == 0)
 {
-printf("Age: (Nil)");
+printf("Age: (Nil)\n");
 } else
 {
 printf("Age: %f\n", d->age);
@@ -29,7 +29,7 @@ printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 {
 printf("Owner: (Nil)");
-} else
+} else 
 {
 printf("Owner: %s\n", d->owner);
 }

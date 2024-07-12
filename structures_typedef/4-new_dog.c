bd786904a9cd9b9043204fb1544593ee10993149
gdbmrm <>
2024-07-12 15:01:18 +0000

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdio.h>
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int i = 0;
+	int len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	ar = malloc((len + 1) * sizeof(char));
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		ar[i] = str[i];
+		i++;
+	}
+	ar[i] = '\0';
+	return (ar);
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t will;
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	will->name = _strdup(name);
+	will->owner = _strdup(owner);
+	
+}

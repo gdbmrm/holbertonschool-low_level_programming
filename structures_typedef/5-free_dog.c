ad95cbf248411ae17f2443a78b04b0c80731d22b
gdbmrm <>
2024-07-12 19:48:37 +0000

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - fonction qui libere la memoire des chiens
+* Write a function that frees dogs.
+* @d: chien a liberer
+*/
+
+void free_dog(dog_t *d)
+{
+free(d);
+free(d->name);
+free(d->owner);
+}

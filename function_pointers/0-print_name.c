baee613b27c47f98f5f6285866564c2f43b1736c
gdbmrm <>
2024-07-15 15:41:32 +0000

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+* print_name - nom fonction
+* Write a function that prints a name.
+* @name: nom a afficher
+* @f: fonction qui affiche le nom
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

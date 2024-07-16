e6496410900641a373fe8cd1ecff6354091bd374
gdbmrm <>
2024-07-16 12:59:23 +0000

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,23 +1,33 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
+/**
+ * sum_them_all - nom fonction variadique
+ *
+ * Write a function that returns the sum of all its parameters.
+ *
+ * @n: nombre de nombre a additionner
+ * Return: somme des arguments
+ */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int somme = 0;
+	int valeur;
 	va_list args;
-	unsigned int i, somme;
-	double valeur;
 
 	va_start(args, n);
+
 	if (n == 0)
 	{
+		va_end(args);
 		return (0);
 	}
 	for (i = 0; i < n; i++)
 	{
-		valeur = va_arg(args, double);
+		valeur = va_arg(args, int);
 		somme += valeur;
 	}
 	va_end(args);
-	return(somme);
+	return (somme);
 
 }

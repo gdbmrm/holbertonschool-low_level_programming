152eb7eb03d8ac51d085fc03b292d777bbe12f39
gdbmrm <>
2024-07-17 14:37:46 +0000

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,7 +12,6 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int somme = 0;
-	int valeur;
 	va_list args;
 
 	va_start(args, n);
@@ -24,8 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
-		valeur = va_arg(args, int);
-		somme += valeur;
+		somme += va_arg(args, int);
 	}
 	va_end(args);
 	return (somme);

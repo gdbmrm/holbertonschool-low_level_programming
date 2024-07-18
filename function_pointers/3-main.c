2de8f5dc3bfff0c15ade92e154d5ba8e7df66273
gdbmrm <>
2024-07-18 11:16:50 +0000

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,15 +1,36 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <string.h>
-
-int main ()
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: nombre d'args
+ * @argv: liste des arguments
+ * Return: result of operation
+ */
+int main(int argc, char *argv[])
 {
-	int a = 14, b = 4;
-	char *operator = "%";
+	int num1;
+	int num2;
 	int calcul;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-	calcul = get_op_func(operator)(a, b);
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 &&
+	strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0 &&
+		strcmp(argv[2], "%") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	calcul = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", calcul);
 	return (0);
 }

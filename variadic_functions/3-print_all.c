ec954bd197575ca16ab09209ae174d9a280692fa
gdbmrm <>
2024-07-18 16:33:02 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,9 +2,38 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+char print_char(char args)
+{
+    return(char args);
+}
+
+int print_int(int args)
+{
+    return(int args);
+}
+
+float print_float(float args)
+{
+    return(float args);
+}
+
+char *print_string(char *args)
+{
+    return(char *args);
+}
+
 void print_all(const char * const format, ...)
 {
     va_list args;
+    typedef struct
+    {
+        char lettre;
+        (*f)();
+
+    }
+
+
+
     if (format == NULL)
     {
         return;

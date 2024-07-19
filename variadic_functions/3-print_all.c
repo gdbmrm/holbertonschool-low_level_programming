2c123944c5b2e3a217b4096c6e4d65328a57a5ec
gdbmrm <>
2024-07-19 15:54:28 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,76 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 char print_char(char args)
 {
-    return(char args);
+    char c = (char)va_arg(*args, int);
+    printf("%c", c);
 }
 
 int print_int(int args)
 {
-    return(int args);
+    int i = va_arg(*args, int);
+    printf("%d", i);
 }
 
 float print_float(float args)
 {
-    return(float args);
+    double f = va_arg(*args, double);
+    printf("%f", f);
 }
 
 char *print_string(char *args)
 {
-    return(char *args);
+    char *s = va_arg(*args, char *);
+    if (s == NULL)
+    {
+        printf("(nil)");
+
+    } else
+    {
+    printf("%s", s);
+    }
 }
+
+typedef struct printall
+    {
+        char *lettre;
+        void (*f)(va_list args);
+    } printall;
 
 void print_all(const char * const format, ...)
 {
     va_list args;
-    typedef struct
-    {
-        char lettre;
-        (*f)();
-
-    }
-
-
+    
+    printall letter[] = {
+        {'c', print_char},
+        {"i", print_int},
+        {"f", print_float},
+        {"s", print_string},
+        {NULL, NULL}
+    };
+    int i = 0, n = 0;
 
     if (format == NULL)
     {
         return;
     }    
 
-    va_start(args, strlen(format));
-
-
-
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        while (letter[n].lettre != NULL)
+        if (strcmp(format[i], letter[n].lettre) == 0)
+        {
+            letter[n].f(args);
+            break;
+        }
+        i++;
+        n++;
+    }
     va_end(args);
-
-
 
 }

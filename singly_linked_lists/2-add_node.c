7af91c8a59c91e1be8a32a70a877e56a8e5a5aed
gdbmrm <>
2024-07-29 15:06:20 +0000

    2_add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * _strlen - fonction qui renvoie la longeur d'un string
+ *
+ * Write a function that returns the length of a string.
+ *
+ * @s: string d'entrÃe
+ *
+ * Return: longeur du string
+ */
+
+
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
+}
+
+
+/**
 * add_node - fonction qui crée un noeud
 * Write a function that adds a new node
 * at the beginning of a list_t list.
@@ -22,7 +44,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	element->len = strlen(str);
+	element->len = _strlen(str);
 	element->next = *head;
 	*head = element;
 	return (element);

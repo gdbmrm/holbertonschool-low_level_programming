100263d8a78b7952cbac1c49691a3a01f2b44bdd
gdbmrm <>
2024-07-30 13:26:52 +0000

    ex 5 get_dnode_int

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* get_dnodeint_at_index - fonction
+* Write a function that returns the nth node
+*  of a dlistint_t linked list.
+* @head: tete de la liste
+* @index: index du noeud que l'on cherche
+* Return: noeud à l'indice index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current = head;
+
+	if (index <= 0)
+	{
+		return (head);
+	}
+	for (i = 0; i < index; i++)
+	{
+		current = current->next;
+	}
+	return (current);
+}

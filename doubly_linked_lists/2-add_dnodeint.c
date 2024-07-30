48ec8277c2f6cc3087f31aa48589f1aed5ab57dd
gdbmrm <>
2024-07-30 11:24:52 +0000

    ex 2 add_dnodeint

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+* add_dnodeint - fonction
+* Write a function that adds a new node
+* at the beginning of a dlistint_t list.
+* @head: tete de liste
+* @n: element a initialiser
+* Return: nouvelle tete de liste
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *element;
+
+	element = malloc(sizeof(dlistint_t));
+	if (element == NULL)
+	{
+		exit(100);
+	}
+
+	element->n = n;
+	element->prev = NULL;
+
+	if (head == NULL)
+	{
+		element->next = NULL;
+		*head = element;
+	} else
+	{
+		element->next = *head;
+		element->prev = element;
+		*head = element;
+	}
+
+	return (*head);
+}

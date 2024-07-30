535969c5292b2480c6a35fc2b403606bee4ab26d
gdbmrm <>
2024-07-30 20:52:31 +0000

    changement 8-delete_dnodeint

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,14 +4,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     unsigned int i;
     dlistint_t *current = *head;
-    dlistint_t *element;
+    dlistint_t *element = *head;
 
-    for (i = 0; i < index; i++)
+    for (i = 0; i <= index && current != NULL; i++)
     {
         current = current->next;
     }
-    current->next = element->next;
-    element->next->prev = element->prev;
-    free(element);
+    current = element->next;
+    element->next = current->next;
+    current->next->prev = element;
+    free(current);
     return(1);
 }

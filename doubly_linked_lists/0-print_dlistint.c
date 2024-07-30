1b4460666e20fe23b3b04974c5f5905299b41231
gdbmrm <>
2024-07-30 10:16:37 +0000

    betty 0-print_dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+* print_dlistint - nom fonction
+* Write a function that prints all the elements of a dlistint_t list.
+* @h: noeud a liberer
+* Return: nombre d'elements de la double liste chainee
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;

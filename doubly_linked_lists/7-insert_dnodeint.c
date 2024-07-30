a342a8ebddc06bb29a450fabf72aacd036cebc52
gdbmrm <>
2024-07-30 18:08:34 +0000

    ex 7 insert_dnodeint

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - fonction
+* Write a function that inserts a new node at a given position.
+* @h: tete de liste
+* @idx: indice ou l'on doit créer le nom
+* @n: valeur du noeud
+* Return: nouveau noeud a l'indice idx
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *current = *h;
+	dlistint_t *element;
+
+	if (idx <= 0)
+	{
+		return (*h);
+	}
+	for (i = 0; i < (idx - 1); i++)
+	{
+		current = current->next;
+	}
+	element = malloc(sizeof(dlistint_t));
+	element->n = n;
+	element->next = current->next;
+	element->prev = current;
+	current->next = element;
+	return (element);
+}

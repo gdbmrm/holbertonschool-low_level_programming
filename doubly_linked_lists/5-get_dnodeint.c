eeb54da66ad40b3daa0ba09ae7243f55cfad42c7
gdbmrm <>
2024-07-31 08:39:49 +0000

    correction des exos

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,10 +12,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 	dlistint_t *current = head;
 
-	if (index <= 0)
-	{
-		return (head);
-	}
 	for (i = 0; i < index; i++)
 	{
 		current = current->next;

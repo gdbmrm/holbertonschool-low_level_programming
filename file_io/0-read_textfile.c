cbceec0dbe0a640e9d347baf8f9085b29021102c
gdbmrm <>
2024-08-01 14:42:40 +0000

    ex 0 p3

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,18 +3,20 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int res_open = 0;
+	int res_write
 	
 	if (filename == NULL)
 	{
 		return (NULL);
 	}
 
-	int res_open = open(filename , O_WRONLY );
+	res_open = open(filename , O_WRONLY );
 
 	if (res_open == -1)
 	{
 		return (0);
 	}
-
-	write(POSIX, )
+	ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
+    if (bytesRead == -1) {
+	res_write = write(STDOUT_FILENO,  )
 }

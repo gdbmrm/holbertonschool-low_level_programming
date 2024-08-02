c3bf076da8e69e1ce418720fb744b8a16750d823
gdbmrm <>
2024-08-02 10:46:28 +0000

    correction 7-insert_dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,32 +11,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *current = *h;
-	dlistint_t *element;
+	dlistint_t *current = *h, *element;
 
 	if (idx == 0)
 	{
 		element = malloc(sizeof(dlistint_t));
 		if (element == NULL)
-		{
 			return (NULL);
-		}
 		element->n = n;
-        element->next = *h;
-        element->prev = NULL;
-        if (*h != NULL)
-        {
-            (*h)->prev = element;
-        }
-        *h = element;
-        return (element);
+		element->next = *h;
+		element->prev = NULL;
+		if (*h != NULL)
+		{
+			(*h)->prev = element;
+		}
+		*h = element;
+		return (element);
 	}
+
 	for (i = 0; i < (idx - 1); i++)
 	{
 		if (current  == NULL)
-		{
 			return (NULL);
-		}
 		current = current->next;
 	}
 	element = malloc(sizeof(dlistint_t));
@@ -48,9 +44,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	element->next = current->next;
 	element->prev = current;
 	if (current->next != NULL)
-    {
-        current->next->prev = element;
-    }
+	{
+		current->next->prev = element;
+	}
 	current->next = element;
 	return (element);
 }
--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,13 +5,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     unsigned int i;
     dlistint_t *current = *head;
 
-    for (i = 0; i <= index && current != NULL; i++)
+    for (i = 0; i < index && current != NULL; i++)
     {
         current = current->next;
     }
     current->next = current->next->next;
     current->prev = current;
     current->next->next->prev = current;
-    free(current);
+    free(current->prev);
+    free();
     return(1);
 }

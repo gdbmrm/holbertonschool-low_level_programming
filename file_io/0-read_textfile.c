2c2bcacbe10ba9c5e25b5720de4f9d781efd1628
gdbmrm <>
2024-08-02 09:22:51 +0000

    tache 0 read_text_file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,22 +1,42 @@
 #include "main.h"
-
+/**
+* read_textfile - fonction
+* Write a function that reads a text file
+* and prints it to the POSIX standard output.
+* @filename: fichier
+* @letters: nombre de lettre a afficher
+* Return: nombre de caractere afficher
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int res_open = 0;
-	int res_write
-	
-	if (filename == NULL)
+	int res_write = 0, bytesRead = 0;
+	char *buffer = malloc(letters);
+
+	if (buffer == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 
-	res_open = open(filename , O_WRONLY );
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
+	res_open = open(filename, O_RDWR);
 	if (res_open == -1)
 	{
 		return (0);
 	}
-	ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
-    if (bytesRead == -1) {
-	res_write = write(STDOUT_FILENO,  )
+
+	bytesRead = read(res_open, buffer, letters);
+	if (bytesRead == -1)
+	{
+		return (0);
+	}
+
+	res_write = write(STDOUT_FILENO, buffer, letters);
+
+	close(res_open);
+	return (res_write);
 }
